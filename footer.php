0c84ac56ab5bee9df8e9d8e63f07f574e53e2c7f
Kadir Doğuş Seçkin <kadir.seckin44@gmail.com>
2021-08-31 22:29:49 +0300

    whatsapp ikonu eklendi, htaccess'te bazı düzenlemeler yapıldı

--- footer.php
+++ footer.php
@@ -224,6 +224,16 @@
 <!-- AdminLTE for demo purposes -->
 <script src="dist/js/demo.js"></script>
 
+<script type="text/javascript"> (function () { var options = {
+ whatsapp: "+905314957131",
+ call_to_action: "Whatsapp hattımız",
+ position: "left",
+ }; 
+var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host; 
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 
+    url + '/widget-send-button/js/init.js'; 
+s.onload = function () { WhWidgetSendButton.init(host, proto, options); }; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })(); </script>
+
 
                
      
